28f57ac3d4d4c81ecb591fc89946ad55d55af742
Tan Li Hau <lhtan93@gmail.com>
2016-03-29 21:06:14 +0800

    socket

--- server.js
+++ server.js
@@ -4,37 +4,39 @@ var pty = require('pty.js');
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  // Create terminal
-  var term = pty.spawn('sh', [], {
-    name: 'xterm-color',
-    cols: 80,
-    rows: 30,
-    cwd: process.env.HOME,
-    env: process.env
-  });
-  // Listen on the terminal for output and send it to the client
-  term.on('data', function(data){
-    console.log(data);
-    socket.emit('output', data);
-  });
-  // Listen on the client and send any input to the terminal
-  socket.on('input', function(data){
-     console.log('input: ' + data);
-     term.write(data);
-  });
-  // When socket disconnects, destroy the terminal
-  socket.on("disconnect", function(){
-     term.destroy();
-     console.log("bye");
-  });
+    console.log('a user connected');
+    // Create terminal
+    var term = pty.spawn('bash', [], {
+        name: 'xterm',
+        cols: 80,
+        rows: 30,
+        cwd: process.cwd(),
+        env: process.env,
+        // cwd: __dirname, //process.env.HOME,
+    });
+    // Listen on the terminal for output and send it to the client
+    term.on('data', function(data){
+        console.log('output: ' + data);
+        socket.emit('output', data);
+    });
+    // Listen on the client and send any input to the terminal
+    socket.on('input', function(data){
+        console.log('input: ' + data);
+        term.write(data);
+    });
+    // When socket disconnects, destroy the terminal
+    socket.on("disconnect", function(){
+        term.destroy();
+        console.log("bye");
+    });
+    console.log(term);
+    term.write("echo 'A'");
 });
 
-
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(8000, function(){
-  console.log('listening on *:8000');
+    console.log('listening on *:8000');
 });
